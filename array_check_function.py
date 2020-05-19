ac23318df0714093920fa811735b75bbd225affd
ironhopper <36726087+ironhopper@users.noreply.github.com>
2020-05-19 15:10:24 +0900

    Update array_check_function.py
    df_concat.replace(' NaN', np.NaN) X
    df_concat.replace(np.nan, '', inplace=True) O

--- array_check_function.py
+++ array_check_function.py
@@ -2,84 +2,89 @@
 # sys.path.append('mlstudy')
 # from array_check_function import df,dfx,dfn,dfnx
 
-import pandas
-import numpy
+import pandas as pd
+import numpy as np
 
 # naming considering / dfm? dfs? multi or single
 tab = '__'
 
 def df1(x):
-  x = pandas.DataFrame(x)
+  x = pd.DataFrame(x)
   display(x)
 
 def df2(x,y):
   row=x.shape[0]
-  x = pandas.DataFrame(x)
-  y = pandas.DataFrame(y)
+  x = pd.DataFrame(x)
+  y = pd.DataFrame(y)
   blank = ['']*row
-  blank = pandas.DataFrame(blank,columns=[tab])
-  df_concat = pandas.concat([x,blank,y], axis=1)
+  blank = pd.DataFrame(blank,columns=[tab])
+  df_concat = pd.concat([x,blank,y], axis=1)
   display(df_concat)
 
 # no-extension , number of parameters is not limited, 2 or 3, whatever you want.
 # ex) df(A,B,C,D,...,Z...)
 # of course you just put one parameter. 
 def df(*x):
-  pandas.reset_option('display.max_columns')
-  pandas.reset_option('display.max_rows')
-
+  pd.reset_option('display.max_columns')
+  pd.reset_option('display.max_rows')
   leng = len(x)
   df_concat = []
   for i in range(leng):
     row=len(x[0])
     blank = ['']*row
-    blank = pandas.DataFrame(blank,columns=[tab])
-    xx = pandas.DataFrame(x[i])
+    blank = pd.DataFrame(blank,columns=[tab])
+    xx = pd.DataFrame(x[i])
     if(i==0):
       df_concat = xx
     else:
-      df_concat = pandas.concat([df_concat,blank,xx], axis=1)
+      df_concat = pd.concat([df_concat,blank,xx], axis=1)
+  # df_concat.replace(Null, 'test')
+  # df_concat.fillna(0)
+  # df_concat.info()
+  df_concat.replace(np.nan, '', inplace=True)
   display(df_concat)
 
 def dfn(*x): 
-  pandas.reset_option('display.max_columns')
-  pandas.reset_option('display.max_rows')
-
+  pd.reset_option('display.max_columns')
+  pd.reset_option('display.max_rows')
   leng = len(x)
   df_concat = []
   for i in range(leng):
     row=len(x[0])
     blank = ['']*row
     tabn = '('+str(i+1)+')'
-    blank = pandas.DataFrame(blank,columns=[tabn])
-    xx = pandas.DataFrame(x[i])
+    blank = pd.DataFrame(blank,columns=[tabn])
+    xx = pd.DataFrame(x[i])
+    # xx.replace(' NaN','')
     if(i==0):
-      df_concat = pandas.concat([xx,blank], axis=1)
+      df_concat = pd.concat([xx,blank], axis=1)
     else:
-      df_concat = pandas.concat([df_concat,xx,blank], axis=1)
+      df_concat = pd.concat([df_concat,xx,blank], axis=1)
+  df_concat.replace(np.nan, '', inplace=True)
   display(df_concat)
 
 # extension
 def dfx(*x):
-  pandas.set_option('display.max_columns', None)
-  pandas.set_option('display.max_rows', None)
+  pd.set_option('display.max_columns', None)
+  pd.set_option('display.max_rows', None)
 
   leng = len(x)
   df_concat = []
   for i in range(leng):
     row=len(x[0])
     blank = ['']*row
-    blank = pandas.DataFrame(blank,columns=[tab])
-    xx = pandas.DataFrame(x[i])
+    blank = pd.DataFrame(blank,columns=[tab])
+    xx = pd.DataFrame(x[i])
     if(i==0):
       df_concat = xx
     else:
-      df_concat = pandas.concat([df_concat,blank,xx], axis=1)
+      df_concat = pd.concat([df_concat,blank,xx], axis=1)
+  df_concat.replace(np.nan, '', inplace=True)
   display(df_concat)
 
 def dfnx(*x):
-  pandas.set_option('display.max_columns', None)
-  pandas.set_option('display.max_rows', None)
+  pd.set_option('display.max_columns', None)
+  pd.set_option('display.max_rows', None)
 
   leng = len(x)
   df_concat = []
@@ -87,16 +92,27 @@ def dfnx(*x):
     row=len(x[0])
     blank = ['']*row
     tabn = '('+str(i+1)+')'
-    blank = pandas.DataFrame(blank,columns=[tabn])
-    xx = pandas.DataFrame(x[i])
+    blank = pd.DataFrame(blank,columns=[tabn])
+    xx = pd.DataFrame(x[i])
     if(i==0):
-      df_concat = pandas.concat([xx,blank], axis=1)
+      df_concat = pd.concat([xx,blank], axis=1)
     else:
-      df_concat = pandas.concat([df_concat,xx,blank], axis=1)
+      df_concat = pd.concat([df_concat,xx,blank], axis=1)
+  df_concat.replace(np.nan, '', inplace=True)
   display(df_concat)
 
-# Usage
-# df(X,Y)
-# dfx(X,Y)
-# dfn(X,Y,X)
-# dfnx(X,Y)
+### Usage
+# df(X,Y,Z)
+# dfx(X,Y,Z)
+# dfn(X,Y,Z)
+# dfnx(X,Y,Z)
+
+### example
+# from sklearn import datasets
+# iris = datasets.load_iris()
+# X = iris.data[:, :2]  # we only take the first two features. whatever you want.
+# Y = iris.target
+# Z = np.array([[1,2],[3,4]])
+# A = np.zeros(100)
+# B = [1,2,3,4]
+# df(X,Y,Z,A,B)
